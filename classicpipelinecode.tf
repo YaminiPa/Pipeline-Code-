2b8abd6b561b72bc2616a86c7a87b9ff4509727e
unknown <yamini.pandey@ankit11mayoutlook.onmicrosoft.com>
2025-08-15 19:22:10 +0530

    h

--- classicpipelinecode.tf
+++ classicpipelinecode.tf
@@ -70,3 +70,8 @@ resource "azurerm_resource_group" "example5" {
   name     = "rebase2"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "example6" {
+  name     = "squash"
+  location = "West Europe"
+}
