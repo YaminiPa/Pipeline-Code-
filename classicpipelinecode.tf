72edfb6d63140b2891c9c6c79fae2adca9bb07a7
unknown <yamini.pandey@ankit11mayoutlook.onmicrosoft.com>
2025-08-15 18:37:20 +0530

    final

--- classicpipelinecode.tf
+++ classicpipelinecode.tf
@@ -1,4 +1,11 @@
 terraform {
+  
+  backend "azurerm" {
+    resource_group_name  = "example-resources1"
+    storage_account_name = "tfstateexample1"
+    container_name       = "tfstate"
+    key                  = "classicpipelinecode.terraform.tfstate"
+  }
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
@@ -7,18 +14,32 @@ terraform {
   }
 }
 
+
+
+
 provider "azurerm" {
   features {}
   subscription_id = "b397c2d0-369a-47dd-9100-9467b7447185"
 }
 
 
+
+
+
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources1"
+  name     = "example-resources11"
   location = "West Europe"
 }
 
 resource "azurerm_resource_group" "example1" {
-  name     = "example-resources2"
+  name     = "example-resources21"
   location = "West Europe"
+}
+
+resource "azurerm_storage_account" "stg" {
+  name                     = "tfstateexample1"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
