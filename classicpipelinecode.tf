a4fef407e53b8fb7f8c6c5d3f90eca5f80e7ce60
unknown <yamini.pandey@ankit11mayoutlook.onmicrosoft.com>
2025-08-15 18:54:50 +0530

    final

--- classicpipelinecode.tf
+++ classicpipelinecode.tf
@@ -43,3 +43,16 @@ resource "azurerm_storage_account" "stg" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+resource "azrerm_resource_group" "example2" {
+  name     = "example-resources31"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "stg1" {
+  name                     = "tfstateexample2"
+  resource_group_name      = azurerm_resource_group.example1.name
+  location                 = azurerm_resource_group.example1.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
